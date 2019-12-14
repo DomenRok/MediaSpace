05683e4243ef221e3c10fbed9ddb5e82a4b4fc1a
Domen Brunček <domen@arvio.si>
2019-12-14 16:41:28 +0100

    added rest-auth, api, updated requirements, added apps to installed settings

--- MediaSpace/MediaSpace/settings.py
+++ MediaSpace/MediaSpace/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'api',
+    'rest_auth',
+    'rest_framework.authtoken',
+    'rest_framework'
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'

87d07785c9c5d1d756c666b3540da6d36739c7d0
premky85 <leon.premky@gmail.com>
2020-01-09 15:26:09 +0100

    Added different recommendation options

--- Recomendations/main.py
+++ Recomendations/main.py
@@ -1,12 +1,18 @@
 from Recomendations import Recommend, SlopOne_Predictor, Cosine_Predictor
+from content.services import RecomendationService
 import random
 
-def favouriteMovies(user, df):
-    uid = Recommend.UserItemData(df, 1000)
-    preds = [SlopOne_Predictor.SlopOnePredictor, Cosine_Predictor.CosinePredictor]
-    pred = random.choice(preds)()
-    rec = Recommend.Recommender(pred)
-    rec.fit(uid)
-    rec_items = rec.recommend(user, 50, False)
 
-    return rec_items.keys()
+class RecomendationService:
+    def favouriteMovies(user):
+        df = RecomendationService.return_dataframe()
+        uid = Recommend.UserItemData(df, 1000)
+        preds = [SlopOne_Predictor.SlopOnePredictor]#, Cosine_Predictor.CosinePredictor]
+        pred = random.choice(preds)()
+        rec = Recommend.Recommender(pred)
+        rec.fit(uid)
+        rec_items = rec.recommend(user, 50, False)
+
+        return rec_items.keys()
+
+

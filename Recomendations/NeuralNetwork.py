89030aee670a7359b5e53a9f32e81b72059b1736
premky85 <leon.premky@gmail.com>
2020-01-09 13:28:37 +0100

    Added different recommendation options

---
+++ Recomendations/NeuralNetwork.py
@@ -0,0 +1,45 @@
+
+
+import numpy as np
+import pandas as pd
+
+class NeuralNetwork:
+
+    # initialization
+    def __init__(self, data, results):
+        self.data = data
+        self.results = results
+        self.weights = len(data[0]) * [0.5]
+
+    # activation function ==> S(x) = 1/1+e^(-x)
+    def sigmoid(self, x, deriv=False):
+        if deriv == True:
+            return x * (1 - x)
+        return 1 / (1 + np.exp(-x))
+
+    # data will flow through the neural network.
+    def feed_forward(self):
+        self.hidden = self.sigmoid(np.dot(self.inputs, self.weights))
+
+    # going backwards through the network to update weights
+    def backpropagation(self):
+        self.error = self.outputs - self.hidden
+        delta = self.error * self.sigmoid(self.hidden, deriv=True)
+        self.weights += np.dot(self.inputs.T, delta)
+
+    # train the neural net for 25,000 iterations
+    def train(self, epochs=25000):
+        for epoch in range(epochs):
+            # flow forward and produce an output
+            self.feed_forward()
+            # go back though the network to make corrections based on the output
+            self.backpropagation()
+            # keep track of the error history over each epoch
+            #self.error_history.append(np.average(np.abs(self.error)))
+            #self.epoch_list.append(epoch)
+
+    # function to predict output on new and unseen input data
+    def predict(self, new_input):
+        prediction = self.sigmoid(np.dot(new_input, self.weights))
+        return prediction
+
--- Recomendations/main.py
+++ Recomendations/main.py
@@ -1,44 +1,14 @@
-import numpy as np
-import pandas as pd
+from Recomendations import Recommend, SlopOne_Predictor, Cosine_Predictor
+import random
 
-class NeuralNetwork:
+def favouriteMovies(user, df):
+    uid = Recommend.UserItemData(df, 1000)
+    preds = [SlopOne_Predictor.SlopOnePredictor, Cosine_Predictor.CosinePredictor]
+    pred = random.choice(preds)()
+    rec = Recommend.Recommender(pred)
+    rec.fit(uid)
+    rec_items = rec.recommend(user, 50, False)
 
-    # initialization
-    def __init__(self, data, results):
-        self.data = data
-        self.results = results
-        self.weights = len(data[0]) * [0.5]
+    return rec_items.keys()
 
-    # activation function ==> S(x) = 1/1+e^(-x)
-    def sigmoid(self, x, deriv=False):
-        if deriv == True:
-            return x * (1 - x)
-        return 1 / (1 + np.exp(-x))
 
-    # data will flow through the neural network.
-    def feed_forward(self):
-        self.hidden = self.sigmoid(np.dot(self.inputs, self.weights))
-
-    # going backwards through the network to update weights
-    def backpropagation(self):
-        self.error = self.outputs - self.hidden
-        delta = self.error * self.sigmoid(self.hidden, deriv=True)
-        self.weights += np.dot(self.inputs.T, delta)
-
-    # train the neural net for 25,000 iterations
-    def train(self, epochs=25000):
-        for epoch in range(epochs):
-            # flow forward and produce an output
-            self.feed_forward()
-            # go back though the network to make corrections based on the output
-            self.backpropagation()
-            # keep track of the error history over each epoch
-            #self.error_history.append(np.average(np.abs(self.error)))
-            #self.epoch_list.append(epoch)
-
-    # function to predict output on new and unseen input data
-    def predict(self, new_input):
-        prediction = self.sigmoid(np.dot(new_input, self.weights))
-        return prediction
-
-    

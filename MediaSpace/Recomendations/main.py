bb7d86239e05108d4f43d47f393a1b13aaeef991
premky85 <leon.premky@gmail.com>
2020-01-10 14:57:40 +0100

    Added different recommendation options

--- MediaSpace/Recomendations/main.py
+++ MediaSpace/Recomendations/main.py
@@ -1,18 +1,20 @@
 from Recomendations import Recommend, SlopOne_Predictor, Cosine_Predictor
-from content.services import RecomendationService
+from content.services import DataUtils
 import random
 
 
 class RecomendationService:
     def favouriteMovies(user):
-        df = RecomendationService.return_dataframe()
+        df = DataUtils.return_dataframe()
         uid = Recommend.UserItemData(df, 1000)
         preds = [SlopOne_Predictor.SlopOnePredictor]#, Cosine_Predictor.CosinePredictor]
         pred = random.choice(preds)()
         rec = Recommend.Recommender(pred)
         rec.fit(uid)
         rec_items = rec.recommend(user, 50, False)
-
         return rec_items.keys()
+
+
+rs = RecomendationService.favouriteMovies(3)
 
 

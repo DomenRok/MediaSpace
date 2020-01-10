4a7ff0384f9c5c97da3203950442faf8037bf7bb
Mario <mb1616@student.uni-lj.si>
2020-01-10 20:25:09 +0100

    mp4 video instead of youtube as an example

--- Frontend/mediaspace/src/Components/DashBoardSlider.tsx
+++ Frontend/mediaspace/src/Components/DashBoardSlider.tsx
@@ -61,18 +61,26 @@ export const DashBoardSlider = (props: Props) => {
 
     const videoPlayer = (slide: Slider) => {
         if (supportedExt.has(slide.video.split('.').pop() as string)) {
-            return (
+            return (<>
+                <a className="progression-studios-slider-play-btn afterglow"
+                   href={"#VideoLightbox-" + slide.id}><i
+                    className="fas fa-play"></i></a>
                 <video id={"VideoLightbox-" + slide.id}
                        poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg?v1" width="960"
                        height="540">
                     <source src={slide.video} type="video/mp4"/>
                 </video>
+                </>
             )
         } else {
             if (slide.video.includes("youtube")) {
-                return (
+                return (<>
+                    <a className="progression-studios-slider-play-btn afterglow"
+                       href={"#VideoLightbox-" + slide.id}><i
+                        className="fas fa-play"></i></a>
                     <video id={"VideoLightbox-" + slide.id} width="960" height="540"
                            data-youtube-id={getId(slide.video)}></video>
+                    </>
                 )
             }
         }
@@ -87,9 +95,6 @@ export const DashBoardSlider = (props: Props) => {
                 <div className="progression-studios-slider-display-table">
                     <div className="progression-studios-slider-vertical-align">
                         <div className="container">
-                            <a className="progression-studios-slider-play-btn afterglow"
-                               href={"#VideoLightbox-" + slide.id}><i
-                                className="fas fa-play"></i></a>
                             {videoPlayer(slide)}
                             <div
                                 className="circle-rating-pro"

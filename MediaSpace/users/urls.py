0cd1f74a7c18f4cef35cc0cbacb9c7651cef91e9
Domen Brunček <domen@arvio.si>
2020-01-11 20:14:56 +0100

    added logout view

--- MediaSpace/users/urls.py
+++ MediaSpace/users/urls.py
@@ -7,7 +7,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('', views.UserList.as_view()),
     path('<int:pk>/', views.UserDetail.as_view()),
-    path('login', views.login)
+    path('login', views.login),
+    path('logout', views.logout)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- MediaSpace/users/views.py
+++ MediaSpace/users/views.py
@@ -4,6 +4,8 @@ from rest_framework import generics
 from . import models
 from . import serializers
 
+from users.models import CustomUser
+
 from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
@@ -43,6 +45,23 @@ def login(request):
     return Response({'token': token.key},
                     status=HTTP_200_OK)
 
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def logout(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'},
+                            status=HTTP_404_NOT_FOUND)
+    if user:
+        token = Token.objects.get(user_id=user.id)
+        token.delete()
+    return Response()
 
 
 

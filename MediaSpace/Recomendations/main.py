20a7b9acfbb9f360e1c235c3dd21bd54a6a13f60
Domen Brunček <domen@arvio.si>
2020-01-12 14:28:49 +0100

    added print debug statements

--- MediaSpace/Recomendations/main.py
+++ MediaSpace/Recomendations/main.py
@@ -1,4 +1,4 @@
-from Recomendations import Recommend, SlopOne_Predictor, Cosine_Predictor
+from recomendations import Recommend, SlopOne_Predictor, Cosine_Predictor
 from content.services import DataUtils
 import random
 import MySQLdb as mysql
@@ -9,12 +9,16 @@ class RecomendationService:
     def favouriteMovies(self, user):
 
         df = DataUtils.return_dataframe()
+        print("Vrnena matrika")
         uid = Recommend.UserItemData(df, 1000)
+        print("Matrix read")
         preds = [SlopOne_Predictor.SlopOnePredictor]#, Cosine_Predictor.CosinePredictor]
         pred = random.choice(preds)()
         rec = Recommend.Recommender(pred)
         rec.fit(uid)
+        print("Fit completed")
         rec_items = rec.recommend(user, 50, False)
+        print("recommendation given")
 
         mydb = mysql.connect(
             host="86.61.77.35",
@@ -24,12 +28,15 @@ class RecomendationService:
         )
         cursor = mydb.cursor()
         for idmovie in rec_items.keys():
+            print(idmovie)
             query = ("INSERT INTO content_recommendation(movie_id, person_id) VALUES (%s, %s)")
             cursor.execute(query, (user, rec_items[idmovie]))
 
         mydb.commit()
         cursor.close()
         mydb.close()
+
+RecomendationService().favouriteMovies(71535)
 
 
 

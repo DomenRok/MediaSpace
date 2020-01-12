bcce461e74a4aa5f8f2bd258183d53838d1a3891
premky85 <leon.premky@gmail.com>
2020-01-12 20:15:58 +0100

    Added movie similarity

--- MediaSpace/Recomendations/main.py
+++ MediaSpace/Recomendations/main.py
@@ -8,7 +8,7 @@ import MySQLdb as mysql
 class RecomendationService:
     def favouriteMovies(self, user):
 
-        df = DataUtils.return_dataframe()
+        df = DataUtils.return_dataframe(1000)
         print("Vrnena matrika")
         uid = Recommend.UserItemData(df, 1000)
         print("Matrix read")
@@ -40,7 +40,52 @@ class RecomendationService:
         cursor.close()
         mydb.close()
 
-#RecomendationService().favouriteMovies(71535)
+    def similarMoviesFillAll(self):
+        df = DataUtils.return_dataframe(1000)
+        print("Vrnena matrika")
+        uid = Recommend.UserItemData(df, 1000)
+        print("Matrix read")
+        cosine = Cosine_Predictor.CosinePredictor()
+        cosine.fit(uid)
+        print("fit")
+        matrika = cosine.similarityMatrix
+
+        mydb = mysql.connect(
+            host="86.61.77.35",
+            user="tguda",
+            passwd="0xoEb0*&rBmH",
+            db='MediaSpace'
+        )
+
+        cursor = mydb.cursor()
+
+        similarMovies = matrika.stack().sort_values(ascending=False).reset_index()
+        similarMovies.columns = ["movieID_1", "movieID_2", "similarity"]
+        sim = similarMovies.groupby("movieID_1")
+        for i, group in sim:
+            print(i)
+            query = ("DELETE FROM content_similarity WHERE movie_from_id = {}".format(i))
+            cursor.execute(query)
+            mydb.commit()
+            similar = group.head(10)
+            for _, y in similar.iterrows():
+                query = ("INSERT INTO content_similarity(similarity, movie_from_id, movie_to_id) VALUES (%s, %s, %s)")
+                cursor.execute(query, (y["similarity"], i, y["movieID_2"]))
+
+            mydb.commit()
+
+        cursor.close()
+        mydb.close()
+
+
+
+
+
+
+
+
+RecomendationService().favouriteMovies(71535)
+#RecomendationService().similarMoviesFillAll()
 
 
 

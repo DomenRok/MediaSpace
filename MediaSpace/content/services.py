6601d8372363a127292fc2ab7c61b8e1d7c484fc
Domen Brunček <domen@arvio.si>
2020-01-12 14:44:42 +0100

    added param in dataUtils

--- MediaSpace/content/services.py
+++ MediaSpace/content/services.py
@@ -8,9 +8,9 @@ class DataUtils:
         pass
 
     @staticmethod
-    def return_dataframe():
+    def return_dataframe(n=1000):
         df = pd.DataFrame(columns=["user_id", "movie_id", "rating"])
-        for row in Rating.objects.all():
+        for row in Rating.objects.raw('SELECT * from content_rating group by movie_id having count(*) > {}'.format(n)):
             df = df.append({"user_id": row.person_id, "movie_id": row.movie_id, "rating": row.rating}, ignore_index=True)
         return df
 

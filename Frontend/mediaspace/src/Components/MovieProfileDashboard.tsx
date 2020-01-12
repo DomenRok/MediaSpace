ebf0604db4af31fe8f50295c15bdb85387fb6c12
Mario <mb1616@student.uni-lj.si>
2020-01-12 23:18:47 +0100

    Similar movies

--- Frontend/mediaspace/src/Components/MovieProfileDashboard.tsx
+++ Frontend/mediaspace/src/Components/MovieProfileDashboard.tsx
@@ -1,17 +1,52 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from 'react-router-dom';
 import {SideBarNav} from "./SidebarNav";
 import {connect} from "react-redux";
 import {logout} from "../actions/auth";
 import $ from "jquery";
 import "jquery-asRange";
+import MovieItem from "./MovieItem";
 
 interface MovieDetail{
 	movieinfo: any
 }
 
 const MovieProfileDashboard: React.FC<MovieDetail> = (props: MovieDetail) => {
+	const [similar, setSimilar] = useState([]);
 
+	useEffect(() => {
+		similarFun()
+	}, []);
+
+	const similarFun = () => {
+		let headers = {"Content-Type": "application/json"} as any;
+		let token =  localStorage.getItem("mediaspacetoken");
+		if (token) {
+			headers["Authorization"] = `Token ${token}`;
+		}
+		return fetch("http://localhost:8000/api/v1/content/similarity/"+props.movieinfo.id, {headers,})
+			.then(res => {
+			if (res.status < 500) {
+				return res.json().then(data => {
+					return {status: res.status, data};
+				})
+			} else {
+				console.log("Server Error!");
+				throw res;
+			}
+		})
+			.then(res => {
+				if (res.status === 200) {
+					setSimilar(res.data.results);
+				} else if (res.status === 401 || res.status === 403) {
+					throw new Error(res.data);
+				}
+			});
+	}
+	if (!similar) return null;
+	const similarMovies = similar.map((element: any) => {
+		return <MovieItem title={element.title} rating={8} id={element.id} key={element.id} image={element.thumbnail_url}/>
+	});
         return (
             <>
             <div className="dashboard-container">
@@ -28,72 +63,7 @@ const MovieProfileDashboard: React.FC<MovieDetail> = (props: MovieDetail) => {
 				<div className="movie-details-section">
 					<h2>Similar Movies</h2>
 					<div className="row">
-						<div className="col-12 col-md-6 col-lg-6 col-xl-4">
-							<div className="item-listing-container-skrn">
-								<a href="dashboard-movie-profile.html"><img src="http://via.placeholder.com/500x707" alt="Listing" /></a>
-								<div className="item-listing-text-skrn">
-									<div className="item-listing-text-skrn-vertical-align"><h6><a href="dashboard-movie-profile.html">Bad Neighbors 2</a></h6>
-								      <div
-								        className="circle-rating-pro"
-								        data-value="0.72"
-								        data-animation-start-value="0.72"
-								        data-size="32"
-								        data-thickness="3"
-								        data-fill="{
-								          &quot;color&quot;: &quot;#42b740&quot;
-								        }"
-								        data-empty-fill="#def6de"
-								        data-reverse="true"
-								      ><span className="c42b740">7.2</span></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					
-					
-						<div className="col-12 col-md-6 col-lg-6 col-xl-4">
-							<div className="item-listing-container-skrn">
-								<a href="dashboard-movie-profile.html"><img src="http://via.placeholder.com/500x707" alt="Listing" /></a>
-								<div className="item-listing-text-skrn">
-									<div className="item-listing-text-skrn-vertical-align"><h6><a href="dashboard-movie-profile.html">Star Wars: Rogue One</a></h6>
-								      <div
-								        className="circle-rating-pro"
-								        data-value="0.86"
-								        data-animation-start-value="0.86"
-								        data-size="32"
-								        data-thickness="3"
-								        data-fill="{
-								          &quot;color&quot;: &quot;#42b740&quot;
-								        }"
-								        data-empty-fill="#def6de"
-								        data-reverse="true"
-								      ><span className="c42b740">8.6</span></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					
-						<div className="col-12 col-md-6 col-lg-6 col-xl-4">
-							<div className="item-listing-container-skrn">
-								<a href="dashboard-movie-profile.html"><img src="http://via.placeholder.com/500x707" alt="Listing" /></a>
-								<div className="item-listing-text-skrn">
-									<div className="item-listing-text-skrn-vertical-align"><h6><a href="dashboard-movie-profile.html">The Imitation Game</a></h6>
-								      <div
-								        className="circle-rating-pro"
-								        data-value="0.6"
-								        data-animation-start-value="0.6"
-								        data-size="32"
-								        data-thickness="3"
-								        data-fill="{
-								          &quot;color&quot;: &quot;#ff4141&quot;
-								        }"
-								        data-empty-fill="#ffe1e1"
-								        data-reverse="true"
-								      ><span className="cff4141">6.0</span></div>
-									</div>
-								</div>
-							</div>
-						</div>
+						{similarMovies}
 					</div>
 				
 				</div>
